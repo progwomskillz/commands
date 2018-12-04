8a2a9c0315ff2ee66313037d0ac54b9a3404e0a7
Nik <n.skubak@storytelling-software.com>
2018-12-04 10:45:17 +0300

    Added SendEmailCommandTest

---
+++ tests/commands/send_email_command_tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from commands.send_email_command import SendEmailCommand
+
+
+class SendEmailCommandTest(unittest.TestCase):
+    def setUp(self):
+        from_email = 'MailGun User '
+        from_email += '<postmaster@sandbox594416194b3848e79afe9592cc671be9'
+        from_email += '.mailgun.org>'
+        message = {
+            'from': from_email,
+            'to': 'n.skubak@storytelling-software.com',
+            'subject': 'MailGun',
+            'text': 'Hello',
+        }
+        self.command = SendEmailCommand(message)
+
+    def test_execute(self):
+        self.assertFalse(self.command.execute())
+
+
+if __name__ == '__main__':
+    unittest.main()
