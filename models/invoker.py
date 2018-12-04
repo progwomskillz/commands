0f0052527aca77f524dae7f90489d5b0fbafc944
Nik <n.skubak@storytelling-software.com>
2018-12-04 10:10:10 +0300

    Added Invoker model

---
+++ models/invoker.py
@@ -0,0 +1,17 @@
+class Invoker:
+    def __init__(self, commands):
+        self.commands = commands
+        self.command_history = []
+
+    def execute_commands(self):
+        for command in self.commands:
+            self.__execute_command(command)
+
+    def __execute_command(self, command):
+        if command.execute():
+            self.command_history.append(command)
+
+    def undo_commands(self):
+        for i in range(len(self.command_history)):
+            command = self.command_history.pop()
+            command.undo()
