7c8ef19815b31610702f63ca0d02edd0fb64f058
Nik <n.skubak@storytelling-software.com>
2018-12-04 11:01:13 +0300

    Fixed from_email string in the SendEmailCommandTest

--- tests/commands/send_email_command_tests.py
+++ tests/commands/send_email_command_tests.py
@@ -5,9 +5,8 @@ from commands.send_email_command import SendEmailCommand
 
 class SendEmailCommandTest(unittest.TestCase):
     def setUp(self):
-        from_email = 'MailGun User '
-        from_email += '<postmaster@sandbox594416194b3848e79afe9592cc671be9'
-        from_email += '.mailgun.org>'
+        from_email = 'MailGun User <postmaster@'
+        from_email += 'sandbox594416194b3848e79afe9592cc671be9.mailgun.org>'
         message = {
             'from': from_email,
             'to': 'n.skubak@storytelling-software.com',
