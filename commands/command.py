6634f5a8fc19ebada356cdef126f4c84e82d5764
Nik <n.skubak@storytelling-software.com>
2018-12-04 09:57:33 +0300

    Added command abstract class

---
+++ commands/command.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+from exceptions.command_cannot_be_undone import CommandCannotBeUndone
+
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self):
+        pass
+
+    def undo(self):
+        raise CommandCannotBeUndone('This command cannot be undone')
