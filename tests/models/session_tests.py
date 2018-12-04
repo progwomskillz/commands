e5d7e57859ba7b8526e71670dc12576df6d07075
Nik <n.skubak@storytelling-software.com>
2018-12-04 10:36:34 +0300

    Added SessionTest

---
+++ tests/models/session_tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+from models.session import Session
+from models.user import User
+
+
+class SessionTest(unittest.TestCase):
+    def setUp(self):
+        self.user = User(first_name='Test', last_name='1')
+        self.session = Session()
+
+    def test_add(self):
+        self.session.add(self.user)
+
+    def test_delete(self):
+        self.session.delete(self.user)
+
+
+if __name__ == '__main__':
+    unittest.main()
