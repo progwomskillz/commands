4599d1d7a0f629d12d2c1b0112b74e33278d57a3
Nik <n.skubak@storytelling-software.com>
2018-12-06 14:08:42 +0300

    Refactored WriteToDbCommand

--- commands/write_to_db_command.py
+++ commands/write_to_db_command.py
@@ -1,15 +1,15 @@
-from .command import Command
+from commands.command import Command
 
 
 class WriteToDbCommand(Command):
-    def __init__(self, session, object_for_db):
-        self.session = session
-        self.object_for_db = object_for_db
+    def __init__(self, db_service, object_to_db):
+        self.db_service = db_service
+        self.object_to_db = object_to_db
 
     def execute(self):
-        session_result = self.session.add(self.object_for_db)
-        setattr(self, 'object_from_db', session_result)
+        db_service_result = self.db_service.save(self.object_to_db)
+        self.object_from_db = db_service_result
         return True
 
     def undo(self):
-        self.session.delete(self.object_from_db)
+        self.db_service.delete(self.object_from_db)
