c225a11c82d11e36d7d4c0c95236c23b2d6d2166
Nik <n.skubak@storytelling-software.com>
2018-12-06 16:23:34 +0300

    Temp refactor commands tests

--- tests/commands/send_email_command_tests.py
+++ tests/commands/send_email_command_tests.py
@@ -1,22 +1,25 @@
 import unittest
+from unittest.mock import MagicMock
 
 from commands.send_email_command import SendEmailCommand
+from models.email_service import EmailService
 
 
 class SendEmailCommandTest(unittest.TestCase):
     def setUp(self):
-        from_email = 'MailGun User <postmaster@'
-        from_email += 'sandbox594416194b3848e79afe9592cc671be9.mailgun.org>'
+        email_service = EmailService()
         message = {
-            'from': from_email,
-            'to': 'n.skubak@storytelling-software.com',
-            'subject': 'MailGun',
-            'text': 'Hello',
+            'from': 'Nik <admin@example.com',
+            'to': 'user@example.com',
+            'subject': 'Test',
+            'message': 'Its worked!'
         }
-        self.command = SendEmailCommand(message)
+        self.command = SendEmailCommand(email_service, message)
 
     def test_execute(self):
-        self.assertFalse(self.command.execute())
+        self.command.execute = MagicMock(return_value=False)
+        executed = self.command.execute()
+        self.assertFalse(executed)
 
 
 if __name__ == '__main__':
--- tests/commands/write_to_db_command_tests.py
+++ tests/commands/write_to_db_command_tests.py
@@ -1,18 +1,20 @@
 import unittest
+from unittest.mock import MagicMock
 
 from commands.write_to_db_command import WriteToDbCommand
-from models.db_service import Session
+from models.db_service import DBService
 from models.user import User
 
 
 class WriteToDbCommandTest(unittest.TestCase):
     def setUp(self):
-        session = Session()
-        user = User(first_name='Test', last_name='2')
-        self.command = WriteToDbCommand(session, user)
+        db_service = DBService()
+        user = User(first_name='Nik', last_name='S.')
+        self.command = WriteToDbCommand(db_service, user)
 
     def test_execute(self):
-        self.assertTrue(self.command.execute())
+        executed = self.command.execute()
+        self.assertTrue(executed)
 
     def test_undo(self):
         self.command.undo()
