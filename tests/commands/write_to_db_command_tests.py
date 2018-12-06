bee239cdb847975589d39a7563ffc8388cf8bd27
Nik <n.skubak@storytelling-software.com>
2018-12-06 16:45:12 +0300

    Fixed WriteToDbCommandTest

--- tests/commands/write_to_db_command_tests.py
+++ tests/commands/write_to_db_command_tests.py
@@ -14,6 +14,7 @@ class WriteToDbCommandTest(unittest.TestCase):
         db_service.delete = MagicMock(return_value=True)
         user = User(first_name='Nik', last_name='S.')
         self.command = WriteToDbCommand(db_service, user)
+        self.command.object_from_db = returned_user
 
     def test_execute(self):
         executed = self.command.execute()
