7776f33770d121a351eb37233aa8d06faf06cbe3
Nik <n.skubak@storytelling-software.com>
2018-12-11 14:05:13 +0300

    Changed WriteToDbCommand

--- commands/write_to_db_command.py
+++ commands/write_to_db_command.py
@@ -2,15 +2,12 @@ from commands.command import Command
 
 
 class WriteToDbCommand(Command):
-    def __init__(self, db_service, object_to_db):
+    def __init__(self, db_service):
         self.db_service = db_service
-        self.object_to_db = object_to_db
 
     def execute(self):
-        db_service_result = self.db_service.save(self.object_to_db)
-        self.object_from_db = db_service_result
+        self.db_service.save()
         return True
 
     def undo(self):
-        self.db_service.delete(self.object_from_db)
-        return False
+        self.db_service.delete()
