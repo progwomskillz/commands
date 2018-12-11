ec845199d5ad840e72d8a7bafd778615c01c132f
Nik <n.skubak@storytelling-software.com>
2018-12-11 16:00:56 +0300

    Refactored RabbitWrapper

--- models/rabbit_wrapper.py
+++ models/rabbit_wrapper.py
@@ -1,21 +1,23 @@
 import json
-import os
 
 import pika
 
-from exceptions.env_var_not_set import EnvVarNotSet
-from exceptions.command_runtime_error import CommandRuntimeError
-from models.user import User
-from models.db_service import DBService
+from models.environment_settings import EnvironmentSettings
+from models.db.user import User
+from models.db.engine_maker import EngineMaker
+from models.db.session_maker import SessionMaker
+from models.services.db_service import DBService
 from commands.write_to_db_command import WriteToDbCommand
-from models.email_service import EmailService
+from models.services.email_service import EmailService
 from commands.send_email_command import SendEmailCommand
 from models.invoker import Invoker
+from exceptions.commands.runtime_error import RuntimeError
 
 
-class RabbitWrapper:
+class RabbitWrapper(EnvironmentSettings):
     def __init__(self):
-        self.__set_vars_from_env()
+        self.host = super()._get_var_from_env('RABBIT_HOST')
+        self.queue = super()._get_var_from_env('RABBIT_QUEUE')
 
         connection_params = pika.ConnectionParameters(host=self.host)
         self.connection = pika.BlockingConnection(connection_params)
@@ -24,50 +26,41 @@ class RabbitWrapper:
         self.channel.queue_declare(queue=self.queue)
 
     def start_consuming(self):
-        self.channel.basic_consume(self.__cb, queue=self.queue, no_ack=True)
+        self.channel.basic_consume(self.__cb, queue=self.queue)
         self.channel.start_consuming()
 
     def send_message(self, message):
         body = json.dumps(message)
-        self.channel.basic_publish(exchange='', routing_key=self.queue, body=body)
+        self.channel.basic_publish(
+            exchange='', routing_key=self.queue, body=body
+        )
         self.connection.close()
 
     def __cb(self, ch, method, properties, body):
         user_dict = json.loads(body)
         user = User(**user_dict)
 
-        commands = []
+        engine = EngineMaker.make_engine()
+        session = SessionMaker.make_session(engine)
 
-        db_service = DBService()
-        command = WriteToDbCommand(db_service, user)
-        commands.append(command)
+        db_service = DBService(session, user)
+        write_to_db_command = WriteToDbCommand(db_service)
 
         email_service = EmailService()
-        message = {}
-        command = SendEmailCommand(email_service, message)
-        commands.append(command)
+        email_message = {}
+        send_email_command = SendEmailCommand(email_service, email_message)
+
+        commands = [write_to_db_command, send_email_command]
 
         invoker = Invoker(commands)
+
         try:
             invoker.execute_commands()
-            print('OK')
-        except CommandRuntimeError:
+            print('Success')
+        except RuntimeError:
             print('Error')
 
-        db_service.close_connection()
+        session.close()
+        engine.dispose()
 
-    def __set_vars_from_env(self):
-        self.host = self.__get_host_from_env()
-        self.queue = self.__get_queue_from_env()
-
-    def __get_host_from_env(self):
-        try:
-            return os.environ['RABBIT_HOST']
-        except KeyError:
-            raise EnvVarNotSet('Set RABBIT_HOST environment variable')
-
-    def __get_queue_from_env(self):
-        try:
-            return os.environ['RABBIT_QUEUE']
-        except KeyError:
-            raise EnvVarNotSet('Set RABBIT_QUEUE environment variable')
+        ch.basic_ack(delivery_tag=method.delivery_tag)
